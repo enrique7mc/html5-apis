1007ece4014d902deefe6e4f965e4e5301fd2678
enrique7mc <enrique7mc@gmail.com>
2016-07-09 14:12:01 -0500

    refactor coords

--- geolocation/index.js
+++ geolocation/index.js
@@ -1,27 +1,27 @@
 var output;
 
 function geoFindMe() {
-    output = document.getElementById("out");
+    output = document.getElementById('out');
     if (!existeGeolocation()){
-        output.innerHTML = "<p>Tu navegador no soporta Geolocation</p>";
+        output.innerHTML = '<p>Tu navegador no soporta Geolocation</p>';
         return;
     }
 
-    output.innerHTML = "<p>Determinando ubicación...</p>";
+    output.innerHTML = '<p>Determinando ubicación...</p>';
     navigator.geolocation.getCurrentPosition(onSuccess, onError);
 }
 
 function existeGeolocation() {
-    return "geolocation" in navigator;
+    return 'geolocation' in navigator;
 }
 
 function onSuccess(position) {
     var latitude  = position.coords.latitude;
     var longitude = position.coords.longitude;
-    output.innerHTML = '<p>Latitud: ' + latitude + '° <br />Longitud: ' + longitude + '°</p>';
+    output.innerHTML = '<p>Latitud: ' + latitude.toFixed(2) + '° <br />Longitud: ' + longitude.toFixed(2) + '°</p>';
 
     var img = new Image();
-    img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=15&size=400x400&sensor=false&key=AIzaSyAEQ75bnlcAIqtR84LqAeNQo98Gm4cBOEg";
+    img.src = 'https://maps.googleapis.com/maps/api/staticmap?center=' + latitude + ',' + longitude + '&zoom=15&size=400x400&sensor=false&key=AIzaSyAEQ75bnlcAIqtR84LqAeNQo98Gm4cBOEg';
     output.appendChild(img);
 }
 
