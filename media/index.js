8d5fa91cfa113c456456196529631f9ac63b2e9a
enrique.munguia <enrique.munguia@hulu.com>
2016-07-13 00:46:39 -0500

    add volume control

--- media/index.js
+++ media/index.js
@@ -1,7 +1,9 @@
 var video = document.getElementById("video"),
     playback = document.getElementById("play"),
-    mute = document.getElementById("mute");
+    mute = document.getElementById("mute"),
+    barra = document.getElementById("volumen");
 
+var oldvolume = 1;
 // Listener para play/pause
 playback.addEventListener( "click", function(e) {
     e.preventDefault();
@@ -20,8 +22,18 @@ mute.addEventListener( "click", function(e) {
     if (video.muted) {
         video.muted = false;
         mute.innerHTML = "Mute";
+        video.volume = oldvolume;
+        barra.value = oldvolume;
     } else {
         video.muted = true;
         mute.innerHTML = "Unmute";
+        oldvolume = video.volume;
+        video.volume = 0;
+        barra.value = 0;
     }
 });
+
+// Listener para el volumen
+barra.addEventListener("change", function(e){
+    video.volume = e.target.value;
+}, true);
