95396b7fd1419ed4e59e7ca335be1aa401f03871
enrique7mc <enrique7mc@gmail.com>
2016-07-18 22:45:53 -0400

    set canvas/figuras to initial state

--- canvas/figuras/index.js
+++ canvas/figuras/index.js
@@ -3,46 +3,6 @@ function dibujar() {
     if (canvas.getContext){
         var ctx = canvas.getContext('2d');
 
-        // Rectángulo
-        /*ctx.fillRect(25, 25, 100, 100);
-        ctx.clearRect(45, 45, 60, 60);
-        ctx.strokeRect(50, 50, 50, 50);*/
-
-        // Triángulo completo
-        /*ctx.beginPath();
-        ctx.moveTo(25, 25);
-        ctx.lineTo(105, 25);
-        ctx.lineTo(25, 105);
-        ctx.fill();*/
-
-        // Triángulo contorno
-        /*ctx.beginPath();
-        ctx.moveTo(125, 125);
-        ctx.lineTo(125, 45);
-        ctx.lineTo(45, 125);
-        ctx.closePath();
-        ctx.stroke();*/
-
-        // Arcos
-        /*ctx.beginPath();
-        ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Circulo exterior
-        ctx.moveTo(110, 75);
-        ctx.arc(75, 75, 35, 0, Math.PI, false);  // Boca
-        ctx.moveTo(65, 65);
-        ctx.arc(60, 65, 5, 0, Math.PI * 2, true);  // Ojo izquierdo
-        ctx.moveTo(95, 65);
-        ctx.arc(90, 65, 5, 0, Math.PI * 2, true);  // Ojo derecho
-        ctx.stroke();*/
-
-        // Curvas cúbicas
-        ctx.beginPath();
-        ctx.moveTo(75, 40);
-        ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
-        ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
-        ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
-        ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
-        ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
-        ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
-        ctx.fill();
+        
     }
 }
