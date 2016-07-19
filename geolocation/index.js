ba21385c9b544b4ab65631cbb07ded8576c5767d
enrique7mc <enrique7mc@gmail.com>
2016-07-18 22:53:15 -0400

    set geolocation/index.js to initial state

--- geolocation/index.js
+++ geolocation/index.js
@@ -2,45 +2,17 @@ var output;
 
 function geoFindMe() {
     output = document.getElementById('out');
-    if (!existeGeolocation()){
-        output.innerHTML = '<p>Tu navegador no soporta Geolocation</p>';
-        return;
-    }
-
-    output.innerHTML = '<p>Determinando ubicación...</p>';
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    
 }
 
 function existeGeolocation() {
-    return 'geolocation' in navigator;
+
 }
 
 function onSuccess(position) {
-    var latitude  = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    output.innerHTML = '<p>Latitud: ' + latitude.toFixed(2) + '° <br />Longitud: ' + longitude.toFixed(2) + '°</p>';
-
-    var img = new Image();
-    img.src = 'https://maps.googleapis.com/maps/api/staticmap?center=' + latitude + ',' + longitude + '&zoom=15&size=400x400&sensor=false&key=AIzaSyAEQ75bnlcAIqtR84LqAeNQo98Gm4cBOEg';
-    output.appendChild(img);
+    // key=AIzaSyAEQ75bnlcAIqtR84LqAeNQo98Gm4cBOEg
 }
 
 function onError(error) {
-    var mensaje;
-    switch (error.code) {
-        case 0: // UNKNOWN_ERROR
-            mensaje = 'Error desconocido';
-            break;
-        case 1: // PERMISSION_DENIED
-            mensaje = 'Has elegido no compartir tu ubicación con la aplicación';
-            break;
-        case 2: // POSITION_UNAVAILABLE
-            mensaje = 'No se ha podido determinar tu ubicación';
-            break;
-        case 3: // TIMEOUT
-            mensaje = 'Se ha excedido el tiempo de espera';
-            break;
-    }
 
-    output.innerHTML = mensaje;
 }
